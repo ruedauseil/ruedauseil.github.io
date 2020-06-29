bc1cbc16b0d9b59e5237e48eec0200f573bed998
Natalia Sanchez <nataliasmdev@gmail.com>
2020-06-29 17:09:19 +0200

    Cambios de responsive

--- assets/js/auseil.js
+++ assets/js/auseil.js
@@ -6,22 +6,24 @@ $(document).ready(function ($) {
     // To top button visibility
     // Header visibility
     // =========================
-    $(document).on("scroll", function() {
-        let header = $("header");
-        let headerMenu = $(".header__menu");
+    if (!$(".navbar-toggler").is(":visible")) {
+        $(document).on("scroll", function() {
+            let header = $("header");
+            let headerMenu = $(".header__menu");
 
-        if (window.scrollY > 200) {
-            $("#to-top").fadeIn();
+            if (window.scrollY > 200) {
+                $("#to-top").fadeIn();
 
-            if (!header.hasClass("scrolled")) {
-                headerMenu.hide();
-                headerMenu.fadeIn();
+                if (!header.hasClass("scrolled")) {
+                    headerMenu.hide();
+                    headerMenu.fadeIn();
+                }
+                header.addClass("scrolled");
+            } else {
+                $("#to-top").fadeOut(100);
+
+                header.removeClass("scrolled");
             }
-            header.addClass("scrolled");
-        } else {
-            $("#to-top").fadeOut(100);
-
-            header.removeClass("scrolled");
-        }
-    });
+        });
+    }
 });
